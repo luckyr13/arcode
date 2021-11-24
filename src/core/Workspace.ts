1f624f83fd4c74c999772ceaf8580ce29e41bf22
technodromex <richie.isc@gmail.com>
2021-11-24 17:55:51 -0600

    Add tab indentation and closebrackets

--- src/core/Workspace.ts
+++ src/core/Workspace.ts
@@ -1,24 +1,30 @@
 import {EditorState, Extension} from "@codemirror/state";
 import {EditorView, keymap, highlightActiveLine} from "@codemirror/view";
-import {defaultKeymap} from "@codemirror/commands";
+import {defaultKeymap, indentWithTab} from "@codemirror/commands";
 import { bracketMatching } from "@codemirror/matchbrackets";
 import {javascript} from "@codemirror/lang-javascript";
 import { lineNumbers, gutter } from "@codemirror/gutter";
 import { oneDark } from "@codemirror/theme-one-dark";
 import { defaultHighlightStyle } from "@codemirror/highlight";
+import { closeBrackets } from "@codemirror/closebrackets";
 
 export class Workspace {
 	private _editors: EditorView[] = [];
 	private _extensions: Array<Extension> = [];
 
 	constructor(theme = '') {
-		this._extensions.push(keymap.of(defaultKeymap), bracketMatching());
+		this._extensions.push(
+			keymap.of(defaultKeymap),
+      keymap.of([indentWithTab]),
+			bracketMatching(),
+			closeBrackets(),
+			highlightActiveLine(),
+		);
 
 		if (theme == 'theme-dark' || theme == 'dark-blue') {
 			this._extensions.push(oneDark);
 		} else {
 			this._extensions.push(defaultHighlightStyle.fallback);
-			this._extensions.push(highlightActiveLine());
 		}
 
 		this._extensions.push(
