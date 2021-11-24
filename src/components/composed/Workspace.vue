a663e7b81a8e65c4063c7db4678bd21ea5166c0b
technodromex <richie.isc@gmail.com>
2021-11-24 08:09:40 -0600

    Update workspace and add theme colors

--- src/components/composed/Workspace.vue
+++ src/components/composed/Workspace.vue
@@ -1,9 +1,15 @@
 <template>
-  <div class="arcode-workspace text-center">
-    <img class="logo" alt="arCode" src="@/assets/logo.png">
-    <h4 class="text-center">
-      arCode IDE
-    </h4>
+  <div class="arcode-workspace">
+    <div class="workspace-bg text-center">
+      <img class="logo" alt="arCode" src="@/assets/logo.png">
+      <h4 class="text-center">
+        arCode IDE
+      </h4>
+    </div>
+    <div class="workspace">
+      <div class="tabs"></div>
+      <div class="editor"></div>
+    </div>
   </div>
   
 </template>
@@ -17,15 +23,40 @@ export default defineComponent({
 </script>
 
 <style scoped lang="scss">
+$workspace-tabs-height: 35px;
 .logo {
   width: 20%;
 }
 
 .arcode-workspace {
+  height: 100%;
+  position: relative;
+}
+.workspace-bg {
   display: flex;
   height: 100%;
+  width: 100%;
   justify-content: center;
   align-items: center;
   flex-direction: column;
+  position: absolute;
+  z-index: 0;
+}
+
+.workspace {
+  height: 100%;
+  width: 100%;
+  z-index: 10;
+  position: absolute;
+  z-index: 0;
+}
+
+.workspace .tabs{
+  height: $workspace-tabs-height;
+}
+
+
+.workspace .editor{
+  height: calc(100% - $workspace-tabs-height);
 }
 </style>
