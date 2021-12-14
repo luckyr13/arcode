f520473fb54aab7fcb95190a7784e0cb04eb21d3
technodromex <richie.isc@gmail.com>
2021-12-14 16:56:15 -0600

    Update app details

--- vue.config.js
+++ vue.config.js
@@ -3,12 +3,13 @@ module.exports = {
     config
       .plugin('html')
       .tap(args => {
-      	args[0].title = 'arCode IDE: Arweave development tools';
+      	args[0].title = 'arCode Studio: Online Arweave IDE';
         return args;
       })
   },
   publicPath: './',
   pwa: {
+  	name: 'arCode Studio',
     workboxOptions: {
     	exclude: ['index.html', /\.map$/],
     	//navigateFallback: './'
