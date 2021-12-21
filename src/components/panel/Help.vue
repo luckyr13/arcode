fd5d556bd2104a70a003f010222d2d3626dedfe0
technodromex <richie.isc@gmail.com>
2021-12-21 16:26:59 -0600

    Add useful links

--- src/components/panel/Help.vue
+++ src/components/panel/Help.vue
@@ -2,11 +2,26 @@
 <div class="arcode-main-toolbar-panel-title">
 	Help and info
 </div>
-<div class="accounts-container">
-	<h4>Links:</h4>
-	<ul class="accounts-menu">
+<div class="links-container">
+	<h4>Useful Links:</h4>
+	<ul class="links-menu">
 		<li>
-			<a href="https://github.com/luckyr13/arcode" target="_blank">arCode GitHub</a>
+			<a href="https://github.com/luckyr13/arcode" target="_blank">ArCode GitHub</a>
+		</li>
+		<li>
+			<a href="https://arweave.org" target="_blank">Arweave.org</a>
+		</li>
+		<li>
+			<a href="https://github.com/redstone-finance/redstone-smartcontracts" target="_blank">RedStone SmartContracts SDK</a>
+		</li>
+		<li>
+			<a href="https://arweave.app" target="_blank">Arweave.app Wallet</a>
+		</li>
+		<li>
+			<a href="https://arconnect.io" target="_blank">ArConnect Wallet</a>
+		</li>
+		<li>
+			<a href="https://arwiki.wiki" target="_blank">ArWiki</a>
 		</li>
 	</ul>
 </div>
@@ -24,23 +39,23 @@
 	font-size: 12px;
 	padding-left: 20px;
 }
-.accounts-container {
-	padding: 20px;
+.links-container {
+	padding: 10px;
 	font-size: 12px;
 }
-.accounts-menu {
+.links-menu {
 	padding: 0px;
 	margin-top: 0px;
 	margin-bottom: 0px;
 }
-.accounts-menu li {
+.links-menu li {
 	padding: 10px;
 	font-size: 12px;
 	line-height: 12px;
 	list-style: none;
 	cursor: pointer;
 }
-.accounts-menu li:hover {
+.links-menu li:hover {
 	background-color: rgba(0,0,0,0.3);
 }
 .close-icon {
