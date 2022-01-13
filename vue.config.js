e67ea4d73ed6c5eed16b527a2932b070a78b1b84
technodromex <richie.isc@gmail.com>
2022-01-13 12:37:37 -0600

    Update js libs
    Cool fix added to the webpack config. Thanks to RedStone team support (Ppe)

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,15 @@
+const path = require('path');
+
 module.exports = {
   chainWebpack: config => {
     config
     .plugin('html')
     .tap(args => {
     args[0].title = 'ArCode Studio: Smartweave IDE';
-    return args;
-    })
+      return args;
+    });
+    // Cool fix
+    config.externals({ v8: {}, undici: {} });
   },
   publicPath: './',
   pwa: {
