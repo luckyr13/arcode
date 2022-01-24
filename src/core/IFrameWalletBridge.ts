2e10113d1fe4d7c8419988ac4bcae9e3816989e6
technodromex <richie.isc@gmail.com>
2022-01-24 08:23:58 -0600

    Add bridge validations

--- src/core/IFrameWalletBridge.ts
+++ src/core/IFrameWalletBridge.ts
@@ -2,93 +2,93 @@ import Arweave from 'arweave';
 declare const window: any;
 
 export class IFrameWalletBridge {
-	private _validOriginList: string[];
+  private _validOriginList: string[];
 
-	constructor() {
-		this._validOriginList = [
-			'http://localhost',
-			'https://scanner.redstone.tools',
-			'https://arweave.app'
-		];
-	}
+  constructor() {
+    this._validOriginList = [
+      'http://localhost',
+      'https://scanner.redstone.tools',
+      'https://arweave.app'
+    ];
+  }
 
-	start() {
-		if (window && window.self !== window.top) {
-			// Notify parent window that ArCode is loaded
-			window.top.postMessage({ event: 'arCodeLoaded' }, '*');
-			console.log('IFrame detected ...');
-			return true;
+  start() {
+    if (window && window.self !== window.top) {
+      // Notify parent window that ArCode is loaded
+      window.top.postMessage({ event: 'arCodeLoaded' }, '*');
+      console.log('IFrame detected ...');
+      return true;
     }
 
     return false;
-	}
+  }
 
-	callAPI(payload: Record<string, any>): Promise<string> {
-		if ( !(window && window.self !== window.top) ) {
-			throw Error('ArCode is not inside an iframe :)');
-		}
-		const res = new Promise<string>((resolve, reject) => {
-			// 1. Send request
-			const reqId = Math.round(Math.random() * 100000);
-			window.top.postMessage({ payload, event: 'arcodeAction', id: reqId }, '*');
+  callAPI(payload: Record<string, any>): Promise<string> {
+    if ( !(window && window.self !== window.top) ) {
+      throw Error('ArCode is not inside an iframe :)');
+    }
+    const res = new Promise<string>((resolve, reject) => {
+      // 1. Send request
+      const reqId = Math.round(Math.random() * 100000);
+      window.top.postMessage({ payload, event: 'arcodeAction', id: reqId }, '*');
 
-			// Add a timeout for api call
-			const ms = 900000;
-			const timeout = window.setTimeout(() => {
-				// Remove window listener
-				window.removeEventListener("message", listenerFunction, true);
-				console.error('Bridge timeout!');
-				reject(`Bridge Timeout exceeded!`);
-			}, ms);
+      // Add a timeout for api call
+      const ms = 900000;
+      const timeout = window.setTimeout(() => {
+        // Remove window listener
+        window.removeEventListener("message", listenerFunction, true);
+        console.error('Bridge timeout!');
+        reject(`Bridge Timeout exceeded!`);
+      }, ms);
 
-			const listenerFunction = (event: MessageEvent) => {
-				const origin = event.origin;
-				const data = event.data;
+      const listenerFunction = (event: MessageEvent) => {
+        const origin = event.origin;
+        const data = event.data;
 
-				// Do we trust the sender of this message?
-				if (this._validOriginList.indexOf(origin) < 0) {
-					console.error(`Untrusted origin: ${origin}`);
-					return;
-				}
-				// Validate nonce
-				else if (data.id !== reqId ) {
-					console.error(`Nonce error: ${data.id} !== ${reqId}`);
-					return;
-				}
+        // Do we trust the sender of this message?
+        if (this._validOriginList.indexOf(origin) < 0) {
+          console.error(`Untrusted origin: ${origin}`);
+          return;
+        }
+        // Validate nonce
+        else if (data.id !== reqId ) {
+          console.error(`Nonce error: ${data.id} !== ${reqId}`);
+          return;
+        }
 
-				// Clear timeout on success
-				window.clearTimeout(timeout);
-				// Remove window listener
-				window.removeEventListener("message", listenerFunction, true);
+        // Clear timeout on success
+        window.clearTimeout(timeout);
+        // Remove window listener
+        window.removeEventListener("message", listenerFunction, true);
 
-				// 3. Handle app response
-				if (data.function === 'address') {
-					console.log(`address: ${data.payload}`)
-					resolve(data.payload);
-				} else if (data.function === 'disconnected') {
-					console.log(`Wallet disconnected!`)
-					resolve('disconnected');
-				} else if (data.function === 'handshake') {
-					console.log(`Connection established! [${data.payload}]`);
-					resolve(data.payload);
-				} else if (data.function === 'signedTransaction') {
-					console.log(`Tx signed!`);
-					resolve(data.payload);
-				} else if (data.function === 'error') {
-					console.log(`Bridge error: ${data.payload}`);
-					reject(`Bridge error: ${data.payload}`);
-				}
+        // 3. Handle app response
+        if (data.function === 'address') {
+          console.log(`address: ${data.payload}`)
+          resolve(data.payload);
+        } else if (data.function === 'disconnected') {
+          console.log(`Wallet disconnected!`)
+          resolve('disconnected');
+        } else if (data.function === 'handshake') {
+          console.log(`Connection established! [${data.payload}]`);
+          resolve(data.payload);
+        } else if (data.function === 'signedTransaction') {
+          console.log(`Tx signed!`);
+          resolve(data.payload);
+        } else if (data.function === 'error') {
+          console.log(`Bridge error: ${data.payload}`);
+          reject(`Bridge error: ${data.payload}`);
+        }
 
-				reject('Function not found');
-			};
+        reject('Function not found');
+      };
 
-			// 2. Listen for incoming responses messages
-			window.addEventListener("message", listenerFunction, true);
+      // 2. Listen for incoming responses messages
+      window.addEventListener("message", listenerFunction, true);
 
-		});
+    });
 
-		return res;
-	}
+    return res;
+  }
 
 
 }
--- src/core/Login.ts
+++ src/core/Login.ts
@@ -11,7 +11,7 @@ import { ref } from 'vue';
 import { IFrameWalletBridge } from './IFrameWalletBridge';
 
 export class Login {
-	// User's private key
+  // User's private key
   private _key: ArWallet|null = null;
   private _storage: Storage;
   // User's arweave public address
@@ -19,42 +19,42 @@ export class Login {
   private _cachedProperties = ['MAINADDRESS', 'KEY', 'METHOD'];
   // ArweaveApp
   private _arweaveWebWallet: ArweaveWebWallet = new ArweaveWebWallet({
-		name: 'Arcode Studio',
-		logo: 'https://arweave.net/wJGdli6nMQKCyCdtCewn84ba9-WsJ80-GS-KtKdkCLg'
-	})
+    name: 'Arcode Studio',
+    logo: 'https://arweave.net/wJGdli6nMQKCyCdtCewn84ba9-WsJ80-GS-KtKdkCLg'
+  })
   private _method = '';
 
-	constructor() {
-		this._storage = window.sessionStorage;
-	}
+  constructor() {
+    this._storage = window.sessionStorage;
+  }
 
-	public get key(): ArWallet|null {
-		return this._key ? this._key : 'use_wallet';
-	}
+  public get key(): ArWallet|null {
+    return this._key ? this._key : 'use_wallet';
+  }
 
-	public set key(key: ArWallet|null) {
-		this._key = key;
+  public set key(key: ArWallet|null) {
+    this._key = key;
     this._storage.setItem(this._cachedProperties[1], JSON.stringify(this._key))
-	}
+  }
 
-	public get mainAddress() {
-		return this._mainAddress.value;
-	}
+  public get mainAddress() {
+    return this._mainAddress.value;
+  }
 
 
-	public set mainAddress(address: string) {
-		this._mainAddress.value = address;
-		this._storage.setItem(this._cachedProperties[0], address);
-	}
+  public set mainAddress(address: string) {
+    this._mainAddress.value = address;
+    this._storage.setItem(this._cachedProperties[0], address);
+  }
 
-	public set method(method: string) {
-		this._method = method;
-		this._storage.setItem(this._cachedProperties[2], this._method);
-	}
+  public set method(method: string) {
+    this._method = method;
+    this._storage.setItem(this._cachedProperties[2], this._method);
+  }
 
-	public get method(): string {
-		return this._method;
-	}
+  public get method(): string {
+    return this._method;
+  }
 
 
   setAccount(mainAddress: string, arWallet: ArWallet|null = null, stayLoggedIn= false): void {
@@ -73,59 +73,59 @@ export class Login {
     }
   }
 
-	uploadKeyFile(inputElement: HTMLInputElement, stayLoggedIn: boolean, arweave: Arweave): Promise<string> {
-		const p = new Promise<string>((resolve, reject) => {
-			let address = '';
-			const file = inputElement.files!.length ? 
-				inputElement.files![0] : null;
-			const freader = new FileReader();
-			freader.onload = async () => {
-				const key = JSON.parse(`${freader.result}`);
-				try {
-					address = await arweave.wallets.jwkToAddress(key);
-					this.setAccount(address, key, stayLoggedIn);
-					this.method = 'keyfile';
-					resolve(address);
-				} catch (error) {
-					reject(`Error loading key ${error}`);
-				}
-			};
+  uploadKeyFile(inputElement: HTMLInputElement, stayLoggedIn: boolean, arweave: Arweave): Promise<string> {
+    const p = new Promise<string>((resolve, reject) => {
+      let address = '';
+      const file = inputElement.files!.length ? 
+        inputElement.files![0] : null;
+      const freader = new FileReader();
+      freader.onload = async () => {
+        const key = JSON.parse(`${freader.result}`);
+        try {
+          address = await arweave.wallets.jwkToAddress(key);
+          this.setAccount(address, key, stayLoggedIn);
+          this.method = 'keyfile';
+          resolve(address);
+        } catch (error) {
+          reject(`Error loading key ${error}`);
+        }
+      };
 
-			freader.onerror = () => {
-				reject('Error reading file');
-			}
-			freader.readAsText(file!);
-		});
-	
-		return p;
+      freader.onerror = () => {
+        reject('Error reading file');
+      }
+      freader.readAsText(file!);
+    });
+  
+    return p;
   }
 
   async arConnect(stayLoggedIn: boolean, arweave: Arweave): Promise<string> {
-		if (!window.arweaveWallet) {
-			throw Error('ArConnect not found!');
-		}
-		const address = await arweave.wallets.getAddress();
-		this.setAccount(address, null, stayLoggedIn);
-		this.method = 'arconnect';
-		return address;
+    if (!window.arweaveWallet) {
+      throw Error('ArConnect not found!');
+    }
+    const address = await arweave.wallets.getAddress();
+    this.setAccount(address, null, stayLoggedIn);
+    this.method = 'arconnect';
+    return address;
   }
 
   async arweaveWebWallet(stayLoggedIn: boolean, arweave: Arweave): Promise<string> {
-		this._arweaveWebWallet.setUrl('arweave.app')
-		const address = await this._arweaveWebWallet.connect();
-		this.setAccount(address, null, stayLoggedIn);
-		this.method = 'webwallet';
-		return address;
+    this._arweaveWebWallet.setUrl('arweave.app')
+    const address = await this._arweaveWebWallet.connect();
+    this.setAccount(address, null, stayLoggedIn);
+    this.method = 'webwallet';
+    return address;
   }
 
   async finnie(stayLoggedIn: boolean, arweave: Arweave): Promise<string> {
-		if (!window.arweaveWallet) {
-			throw Error('Finnie Wallet not found!');
-		}
-		const address = await arweave.wallets.getAddress();
-		this.setAccount(address, null, stayLoggedIn);
-		this.method = 'finnie';
-		return address;
+    if (!window.arweaveWallet) {
+      throw Error('Finnie Wallet not found!');
+    }
+    const address = await arweave.wallets.getAddress();
+    this.setAccount(address, null, stayLoggedIn);
+    this.method = 'finnie';
+    return address;
   }
 
   public hijackArweave(arweave: Arweave): void {
@@ -136,67 +136,67 @@ export class Login {
       options?: SignatureOptions
     ): Promise<void> =>
     {
-			await this._arweaveWebWallet.signTransaction(transaction);
-		}
+      await this._arweaveWebWallet.signTransaction(transaction);
+    }
 
     arweave.wallets.getAddress = async (jwk?: JWKInterface | "use_wallet"): Promise<string> =>
     {
-			return this.mainAddress;
-		}
+      return this.mainAddress;
+    }
   }
 
-	public loadSession(stayLoggedIn: boolean, arweave: Arweave) : void{// Check storage 
-		this._storage = window.sessionStorage;
-		if (stayLoggedIn) {
-			this._storage = window.localStorage;
-		}
-		const mainAddress = this._storage.getItem(this._cachedProperties[0]);
-		const key = JSON.parse(this._storage.getItem(this._cachedProperties[1])!);
-		const method = this._storage.getItem(this._cachedProperties[2])!;
+  public loadSession(stayLoggedIn: boolean, arweave: Arweave) : void{// Check storage 
+    this._storage = window.sessionStorage;
+    if (stayLoggedIn) {
+      this._storage = window.localStorage;
+    }
+    const mainAddress = this._storage.getItem(this._cachedProperties[0]);
+    const key = JSON.parse(this._storage.getItem(this._cachedProperties[1])!);
+    const method = this._storage.getItem(this._cachedProperties[2])!;
 
-		if (mainAddress) {
-			this._mainAddress.value = mainAddress;
-			this._key = key;
-			this._method = method;
-			if (this._method === 'webwallet') {
-				this.arweaveWebWallet(stayLoggedIn, arweave);
-			}
-		}
-		console.log('Session data loaded ...');
-	}
+    if (mainAddress) {
+      this._mainAddress.value = mainAddress;
+      this._key = key;
+      this._method = method;
+      if (this._method === 'webwallet') {
+        this.arweaveWebWallet(stayLoggedIn, arweave);
+      }
+    }
+    console.log('Session data loaded ...');
+  }
 
   async logoutBridge() {
     this.removeAccountFromCache();
     this.mainAddress = '';
-		this.key = null;
-		const api = new IFrameWalletBridge();
-		if ((this.method === 'arconnect' || this.method === 'finnie')) {
-			const res = await api.callAPI({
-				method: 'disconnect'
-			});
-			if (res !== 'disconnected') {
-				throw Error('Error on disconnect');
-			}
+    this.key = null;
+    const api = new IFrameWalletBridge();
+    if ((this.method === 'arconnect' || this.method === 'finnie')) {
+      const res = await api.callAPI({
+        method: 'disconnect'
+      });
+      if (res !== 'disconnected') {
+        throw Error('Error on disconnect');
+      }
 
-		} else if (this.method === 'webwallet') {
-			window.arweaveWallet.disconnect();
-		}
-		
-		this.method = '';
-	}
+    } else if (this.method === 'webwallet') {
+      window.arweaveWallet.disconnect();
+    }
+    
+    this.method = '';
+  }
 
-	async logout() {
+  async logout() {
     this.removeAccountFromCache();
     this.mainAddress = '';
-		this.key = null;
-	
-		if ((this.method === 'arconnect' || this.method === 'finnie' || this.method === 'webwallet') &&
-				window.arweaveWallet) {
-			window.arweaveWallet.disconnect();
-		}
-		
-		this.method = '';
-	}
+    this.key = null;
+  
+    if ((this.method === 'arconnect' || this.method === 'finnie' || this.method === 'webwallet') &&
+        window.arweaveWallet) {
+      window.arweaveWallet.disconnect();
+    }
+    
+    this.method = '';
+  }
 
   public hijackArweavePostAPI(arweave: Arweave): void {
     const api = new IFrameWalletBridge();
@@ -207,13 +207,23 @@ export class Login {
       options?: SignatureOptions
     ): Promise<void> =>
     {
+      const txClone = { ...transaction };
+
       const signedTransaction: any =  await api.callAPI({
         method: 'sign',
-				data: {
-					transaction,
-					options
-				}
+        data: {
+          transaction,
+          options
+        }
       });
+
+      // Validations:
+      // Compare signed tx against cloned txs
+      const signedOwner = await arweave.wallets.ownerToAddress(signedTransaction.owner);
+      if (this.mainAddress !== signedOwner) {
+        console.log(this.mainAddress, '!==', signedOwner); 
+        throw Error(`signedTransaction owner mismatches current address!`);
+      }
       
       transaction.setSignature({
         id: signedTransaction.id,
@@ -233,34 +243,34 @@ export class Login {
 
   async finnieBridge(stayLoggedIn: boolean, arweave: Arweave): Promise<string> {
     const api = new IFrameWalletBridge();
-		const address = await api.callAPI({
+    const address = await api.callAPI({
       method: 'getAddress'
     });
     if (address) {
-			this.setAccount(address, null, stayLoggedIn);
-			this.method = 'finnie';
+      this.setAccount(address, null, stayLoggedIn);
+      this.method = 'finnie';
     }
-		return address;
+    return address;
   }
 
   async arConnectBridge(stayLoggedIn: boolean, arweave: Arweave): Promise<string> {
-		const api = new IFrameWalletBridge();
-		const address = await api.callAPI({
+    const api = new IFrameWalletBridge();
+    const address = await api.callAPI({
       method: 'getAddress'
     });
     if (address) {
-			this.setAccount(address, null, stayLoggedIn);
-			this.method = 'arconnect';
+      this.setAccount(address, null, stayLoggedIn);
+      this.method = 'arconnect';
     }
-		return address;
+    return address;
   }
 
   public async isBridgeActive(): Promise<boolean> {
-		const api = new IFrameWalletBridge();
-		return (
-			await api.callAPI({
-				method: 'startHandshake'
-			}) === 'bridgeActive'
-		);
+    const api = new IFrameWalletBridge();
+    return (
+      await api.callAPI({
+        method: 'startHandshake'
+      }) === 'bridgeActive'
+    );
   }
 }
