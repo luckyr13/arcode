6f1a97897496676422579c45abc3a9b4631742d9
technodromex <richie.isc@gmail.com>
2022-01-25 23:18:13 -0600

    Update demo

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.f194334991b0d11a90ed7c4da2320971.js"
+  "precache-manifest.9e625cbbf8046949eadab55559d8e5e0.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "arcode"});
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+import 'core-js/stable'
+import 'regenerator-runtime/runtime'
 import { createApp } from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
